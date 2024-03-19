eb14954817377a11ab8f5df037fe110e0391e996
Barry <barrmull@gmail.com>
2024-03-19 12:16:06 +0100

    fix error on settings page

--- frontend/src/pages/SettingsPage/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage/SettingsPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Box, Container, Typography } from '@mui/material';
-
+import { useSidebarStore } from '@/store/store';
+import { drawerWidth } from '@/components/SideBar/Sidebar';
 
 const Homepage: React.FC = () => {
+    const isSidebarOpen = useSidebarStore((state) => state.isSidebarOpen);
+
     return (
         <Container maxWidth={false} sx={{ width: isSidebarOpen ? `calc(100% - ${drawerWidth.open}px)` : `calc(100% - ${drawerWidth.closed}px)`, p: { xs: 3, xl: 5 }, }}>
             <Box>
